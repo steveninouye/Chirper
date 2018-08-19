dc4fdaa71ca8c0b485028e90e9062079eadeed83
Steven Inouye <steveninouye@msn.com>
2018-08-19 10:19:20 -1000

    set up main files

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Welcome to Chirper ds</h1>
+                </header>
+                <p className="App-intro alert alert-primary">
+                    To get started, edit <code>src/App.js</code> and save to
+                    reload.
+                </p>
+                <p className="alert alert-primary">chirp</p>
+            </div>
+        );
+    }
+}
+
+export default App;
